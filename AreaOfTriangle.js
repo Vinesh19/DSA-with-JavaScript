629f5c4a5c6ac406dad1fc70e62b8f398072611e
Vinesh Wadhwani <vineshwadhwani2000@gmail.com>
2023-04-24 23:13:51 +0530

    AreaOfTriangle

---
+++ AreaOfTriangle.js
@@ -0,0 +1,9 @@
+// Program to Calculate the Area of a Triangle
+
+function AreaOfTri(base, height) {
+    return (base * height)/2;
+}
+
+console.log(AreaOfTri(3,5));
+console.log(AreaOfTri(5,10));
+console.log(AreaOfTri(8,8));
