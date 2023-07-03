8fa0e49f6299851dac565fd70194c323ac480a8c
Vinesh Wadhwani <vineshwadhwani2000@gmail.com>
2023-07-03 23:57:32 +0530

    Replace character

---
+++ replaceChar.js
@@ -0,0 +1,9 @@
+// Program to Replace Characters of a String
+
+const string = 'Mr Red has a red house and a red car';
+
+// replace the characters
+const newText = string.replace('red', 'blue');
+
+// display the result
+console.log(newText);
